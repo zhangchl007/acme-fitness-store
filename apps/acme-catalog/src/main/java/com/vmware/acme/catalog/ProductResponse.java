06cb8e71638178741715bb3566db6d264e53a2ad
maly7 <paul.aly@outlook.com>
2022-04-08 17:14:59 -0400

    fix product endpoint

--- apps/acme-catalog/src/main/java/com/vmware/acme/catalog/ProductResponse.java
+++ apps/acme-catalog/src/main/java/com/vmware/acme/catalog/ProductResponse.java
@@ -8,100 +8,101 @@ import org.springframework.util.StringUtils;
 
 public class ProductResponse {
 
-    private String id;
-    private String imageUrl1;
-    private String imageUrl2;
-    private String imageUrl3;
-    private String name;
-    private String shortDescription;
-    private String description;
-    private Double price;
-    private List<String> tags = new ArrayList<>();
+	private String id;
+	private String imageUrl1;
+	private String imageUrl2;
+	private String imageUrl3;
+	private String name;
+	private String shortDescription;
+	private String description;
+	private Double price;
+	private List<String> tags = new ArrayList<>();
 
-    public ProductResponse(Product product) {
-        id = product.getId();
-        price = product.getPrice();
-        description = product.getDescription();
-        name = product.getName();
-        imageUrl1 = product.getImageUrl1();
-        imageUrl2 = product.getImageUrl2();
-        imageUrl3 = product.getImageUrl3();
-        shortDescription = product.getShortDescription();
+	public ProductResponse(Product product) {
+		id = product.getId();
+		price = product.getPrice();
+		description = product.getDescription();
+		name = product.getName();
+		imageUrl1 = product.getImageUrl1();
+		imageUrl2 = product.getImageUrl2();
+		imageUrl3 = product.getImageUrl3();
+		shortDescription = product.getShortDescription();
 
-        if (StringUtils.hasText(product.getTags())) {
-            tags = Arrays.asList(StringUtils.split(product.getTags(), ","));
-        }
-    }
+		if (StringUtils.hasText(product.getTags())) {
+			tags = Arrays.asList(product.getTags().split(","));
+		}
 
-    public String getId() {
-        return id;
-    }
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getImageUrl1() {
-        return imageUrl1;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setImageUrl1(String imageUrl1) {
-        this.imageUrl1 = imageUrl1;
-    }
+	public String getImageUrl1() {
+		return imageUrl1;
+	}
 
-    public String getImageUrl2() {
-        return imageUrl2;
-    }
+	public void setImageUrl1(String imageUrl1) {
+		this.imageUrl1 = imageUrl1;
+	}
 
-    public void setImageUrl2(String imageUrl2) {
-        this.imageUrl2 = imageUrl2;
-    }
+	public String getImageUrl2() {
+		return imageUrl2;
+	}
 
-    public String getImageUrl3() {
-        return imageUrl3;
-    }
+	public void setImageUrl2(String imageUrl2) {
+		this.imageUrl2 = imageUrl2;
+	}
 
-    public void setImageUrl3(String imageUrl3) {
-        this.imageUrl3 = imageUrl3;
-    }
+	public String getImageUrl3() {
+		return imageUrl3;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setImageUrl3(String imageUrl3) {
+		this.imageUrl3 = imageUrl3;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getShortDescription() {
-        return shortDescription;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
+	public String getShortDescription() {
+		return shortDescription;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public Double getPrice() {
-        return price;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+	public Double getPrice() {
+		return price;
+	}
 
-    public List<String> getTags() {
-        return tags;
-    }
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
 }
