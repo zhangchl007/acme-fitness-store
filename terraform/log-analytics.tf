4bbedd9a6d82920d6f94146af3dd9904e55bba39
kvmw <mshamsi@vmware.com>
2022-04-28 10:30:00 +0200

    Delete all resources during cleanup, including those that are not created by terraform

---
+++ terraform/log-analytics.tf
@@ -0,0 +1,8 @@
+# Log Analiytics Workspace for App Insights
+resource "azurerm_log_analytics_workspace" "asc_workspace" {
+  name                = "${var.project_name}-workspace"
+  location            = azurerm_resource_group.grp.location
+  resource_group_name = azurerm_resource_group.grp.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
